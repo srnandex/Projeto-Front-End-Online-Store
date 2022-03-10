40e122678b345d7f49b841bb5921da230a022555
Artur <fucult96@gmail.com>
2022-03-10 15:58:40 -0300

    adiciona função de getCatories and getProductsFromCategoryAndQuery

--- src/services/api.js
+++ src/services/api.js
@@ -11,7 +11,7 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe;
   try {
     const url = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
     const response = await fetch(url);

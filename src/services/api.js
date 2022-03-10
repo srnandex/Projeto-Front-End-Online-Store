240a18c22571fc963f382b6583bb0f7180632301
Artur <fucult96@gmail.com>
2022-03-10 15:54:10 -0300

    adiciona implementação da função getCategories e getProductsFromCategoryAndQuery

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,23 @@
 export async function getCategories() {
   // Implemente aqui
+  try {
+    const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    return error;
+  }
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  try {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
+    const response = await fetch(url);
+    const result = response.json();
+    return result;
+  } catch (error) {
+    return error;
+  }
 }

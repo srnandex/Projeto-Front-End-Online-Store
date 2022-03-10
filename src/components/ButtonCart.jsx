78dae3fa07ed5e4de8664e22db308d48de4efb55
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-03-10 17:52:48 -0300

    arruma lint - requisito 3

--- src/components/ButtonCart.jsx
+++ src/components/ButtonCart.jsx
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom';
 class ButtonCart extends React.Component {
   render() {
     return (
-      <Link to= "/shoppingCart">
-        <button data-testid="shopping-cart-button">
+      <Link to="/shoppingCart">
+        <button
+          type="button"
+          data-testid="shopping-cart-button"
+        >
           Carrinho
         </button>
       </Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ButtonCart from '../components/ButtonCart';
 
-
 class Home extends React.Component {
   render() {
     return (
